431817d607041125c865b9abad43764aced1ce99
vamsimadhav <vamsimadhav2000@gmail.com>
2023-08-08 20:22:31 +0530

    Added Custom Toolbar

---
+++ app/src/main/java/com/vmh/a7minworkout/ExerciseActivity.kt
@@ -0,0 +1,24 @@
+package com.vmh.a7minworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vmh.a7minworkout.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityExerciseBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolBarExercise)
+
+        if(supportActionBar!= null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding.toolBarExercise.setNavigationOnClickListener{
+            onBackPressed()
+        }
+    }
+}
--- app/src/main/java/com/vmh/a7minworkout/MainActivity.kt
+++ app/src/main/java/com/vmh/a7minworkout/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.vmh.a7minworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.vmh.a7minworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startButton.setOnClickListener {
+            val intent = Intent(this,ExerciseActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
