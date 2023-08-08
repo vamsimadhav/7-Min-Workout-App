3c214bae6679d9f571eb829e008646568a96a2d8
vamsimadhav <vamsimadhav2000@gmail.com>
2023-08-08 22:08:03 +0530

    Added progress bar

--- app/src/main/java/com/vmh/a7minworkout/ExerciseActivity.kt
+++ app/src/main/java/com/vmh/a7minworkout/ExerciseActivity.kt
@@ -2,11 +2,15 @@ package com.vmh.a7minworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.vmh.a7minworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
 
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -20,5 +24,30 @@ class ExerciseActivity : AppCompatActivity() {
         binding.toolbarExercise.setNavigationOnClickListener{
             onBackPressed()
         }
+        setRestView()
     }
+
+    private fun setRestView(){
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+    private fun setRestProgressBar(){
+        binding.pbExercise.progress = restProgress
+
+        restTimer = object: CountDownTimer(10000,1000){
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding.pbExercise.progress = 10 - restProgress
+                binding.tvTimer.text = (10-restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"Timer is completed",Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
 }
